878b2d091f1b128568c3e98a7dd65a076fc21df1
Rutvik <rutvikraut567@gmail.com>
2025-06-06 16:42:43 +0530

    Added Partial Mobile Responsive

--- frontend/src/Components/HomePage/HeroSection.jsx
+++ frontend/src/Components/HomePage/HeroSection.jsx
@@ -3,18 +3,18 @@ import React from 'react'
 
 const HeroSection = () => {
   return (
-    <div className='h-screen bg-primary mb-4 flex justify-center gap-22'>
-        <div className='flex gap-5 flex-col justify-center'>
-            <h2 className='text-white text-6xl font-bold text-wrap'>Lorem ipsum dolor <br /> sit amet</h2>
-            <p className='text-white text-2xl font-medium text-wrap'>Lorem ipsum dolor sit amet</p>
-            <div>
-                <Button className='bg-secondary rounded-4xl hover:bg-secondary cursor-pointer text-nowrap text-xl'>
+    <div className='md:h-screen bg-primary mb-4 flex md:flex-row flex-col justify-center md:gap-22'>
+        <div className='flex gap-5 flex-col md:justify-center'>
+            <h2 className='text-white md:text-left text-center text-6xl font-bold text-wrap'>Lorem ipsum dolor <br /> sit amet</h2>
+            <p className='text-white md:text-left text-center text-2xl font-medium text-wrap'>Lorem ipsum dolor sit amet</p>
+            <div className='flex justify-center md:justify-start'>
+                <Button className='bg-secondary rounded-4xl hover:bg-secondary cursor-pointer text-nowrap text-xl mb-4'>
                    Get Started
                 </Button>
             </div>
         </div>
-        <div className='h-100 flex items-start'>
-            <img className='h-154 mt-23.5' src="/images/background.png" alt="" />
+        <div className='md:h-100 flex'>
+            <img className='h-84 md:h-154 md:mt-23.5' src="/images/background.png" alt="" />
         </div>
     </div>
   )
--- frontend/src/Components/HomePage/OurServiceSection.jsx
+++ frontend/src/Components/HomePage/OurServiceSection.jsx
@@ -4,22 +4,22 @@ import { Button } from "flowbite-react";
 
 const OurServiceSection = () => {
   return (
-    <div className="mt-10 ml-30 mr-30 flex justify-around">
-      <div className="flex flex-col gap-2 w-3/4 justify-center">
+    <div className="mt-10 md:ml-30 md:mr-30 flex md:flex-row flex-col justify-around items-center">
+      <div className="flex flex-col gap-2 w-3/4 md:justify-center md:items-start items-center">
         <h3 className="text-primary font-semibold">Our Services</h3>
         <div className="mb-2">
-          <h2 className="text-wrap text-black text-3xl font-semibold mb-3">
+          <h2 className="md:text-wrap text-nowrap text-black text-3xl font-semibold mb-3 md:text-left text-center">
             What Services <br /> We Provide ?
           </h2>
           <div className="w-24 h-1 bg-secondary rounded-3xl" />
         </div>
         <div>
-          <Button className="bg-secondary rounded-4xl hover:bg-secondary cursor-pointer text-nowrap text-md">
+          <Button className="bg-secondary rounded-4xl hover:bg-secondary cursor-pointer text-nowrap text-md md:visible hidden">
             Get Started
           </Button>
         </div>
       </div>
-      <div className="flex flex-row gap-8">
+      <div className="flex flex-col md:flex-row gap-8">
         {[...Array(3)].map((key) => (
           <ServiceCard key={key} />
         ))}
