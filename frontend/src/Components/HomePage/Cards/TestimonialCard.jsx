115e81012960eac868dbb3e90ebbb7c4adf23e4d
Rutvik <rutvikraut567@gmail.com>
2025-06-12 07:36:55 +0530

    Home Page Completed

--- frontend/src/Components/HomePage/Cards/TestimonialCard.jsx
+++ frontend/src/Components/HomePage/Cards/TestimonialCard.jsx
@@ -3,8 +3,8 @@ import React from "react";
 
 const TestinomialCard = () => {
   return (
-    <div className="rounded-xl shadow-2xl transition duration-500 hover:scale-102 cursor-pointer">
-      <Card className="max-w-sm">
+    <div className="w-full rounded-xl shadow-2xl cursor-pointer">
+      <Card className="max-w-2xs md:max-w-sm">
          <img src="/images/profile.png" alt="" className="w-16" />
         <p className="font-normal text-gray-700 dark:text-gray-400">
             Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.
--- frontend/src/Components/HomePage/OurServiceSection.jsx
+++ frontend/src/Components/HomePage/OurServiceSection.jsx
@@ -5,7 +5,7 @@ import { Link } from "react-router-dom";
 
 const OurServiceSection = () => {
   return (
-    <div className="mt-10 md:ml-30 md:mr-30 flex md:flex-row flex-col justify-around items-center">
+    <div className="mt-20 md:ml-30 md:mr-30 flex md:flex-row flex-col justify-around items-center">
       <div className="flex flex-col gap-2 w-3/4 md:justify-center md:items-start items-center">
         <h3 className="text-primary font-semibold">Our Services</h3>
         <div className="mb-2 flex flex-col md:items-start items-center ">
@@ -29,7 +29,7 @@ const OurServiceSection = () => {
       </div>
       <div className="mt-5 md:hidden">
           <Link to={"services"}>
-            <Button className="bg-secondary rounded-4xl hover:bg-secondary cursor-pointer text-nowrap text-md transition-all duration-300 hover:scale-105 gap-1 hover:gap-2 ">
+            <Button className="bg-secondary rounded-4xl hover:bg-secondary cursor-pointer text-nowrap transition-all duration-300 hover:scale-105 gap-1 hover:gap-2 ">
               Explore More <span className="text-2xl">&#8594;</span>
             </Button>
           </Link>
