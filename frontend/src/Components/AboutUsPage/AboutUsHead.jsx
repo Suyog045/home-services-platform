9159d7cc4e151d5eb03ccba69a891426ee52a1e6
Mahboob Alam <md.mahboobalamansari20@gmail.com>
2025-06-13 16:14:42 +0530

    about us page added completely

--- frontend/src/Components/AboutUsPage/AboutUsHead.jsx
+++ frontend/src/Components/AboutUsPage/AboutUsHead.jsx
@@ -3,7 +3,7 @@ import AboutUs from './../../pages/AboutUs';
 const AboutUsHead =() =>{
     return(
         <div className="bg-primary h-60 w-full flex justify-center items-center ">
-                 <h2 className='text-white md:text-center text-4xl md:text-5xl font-semibold '>About Us </h2>
+                 <h2 className='text-white md:text-center text-4xl md:text-5xl font-semibold mt-12'>About Us </h2>
             
         </div>
     )
