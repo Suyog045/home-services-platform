aa9cde37459c1f8df790a160e9602846f2bdac70
Rutvik <rutvikraut567@gmail.com>
2025-07-21 19:50:12 +0530

    Added Login and Register UI

--- frontend/src/Components/Shared/SharedButton.jsx
+++ frontend/src/Components/Shared/SharedButton.jsx
@@ -1,9 +1,16 @@
 import {Button} from "flowbite-react/components/Button"
+import { useAuthModal } from "../../Providers/AuthModalProvider";
 
 export const SharedButton = ({label}) => {
+  const { isModalOpen, openModal,setModalType } = useAuthModal();
+
+  const handleAuthBtn=()=>{
+    setModalType(label);
+    openModal();
+  }
   return (
     <>
-    <Button className='bg-secondary rounded-4xl hover:bg-secondary cursor-pointer w-24 text-nowrap font-medium'>
+    <Button onClick={handleAuthBtn} className='bg-secondary rounded-4xl hover:bg-secondary cursor-pointer w-24 text-nowrap font-medium'>
         {label}
     </Button>
     </>
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,9 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App'
+import AuthModalProvider from './Providers/AuthModalProvider'
+import { ModalWrapper } from './Components/AuthPage/Wrapper/ModalWrapper'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <AuthModalProvider>
+    <ModalWrapper/>
+      <App />
+  </AuthModalProvider>
 )
