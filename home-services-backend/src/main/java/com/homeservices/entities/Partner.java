d041b85a85d094a057b526fbde4b65b3f47cead2
Suyog Avhad <ssavhad021@gmail.com>
2025-08-01 13:42:16 +0530

    added partner address and mapping with other entities

--- home-services-backend/src/main/java/com/homeservices/entities/Partner.java
+++ home-services-backend/src/main/java/com/homeservices/entities/Partner.java
@@ -3,8 +3,12 @@ package com.homeservices.entities;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -26,7 +30,6 @@ public class Partner extends BaseEntity {
 	private String email;
 	@Column(name="phone_number")
 	private String phoneNumber;
-	private String city;
 	private String password;
 	@Column(name="birth_date")
 	private LocalDate birthDate;
@@ -42,6 +45,18 @@ public class Partner extends BaseEntity {
 	private boolean isDeleted;
 	@Column(name = "deletedAt")
 	private LocalDateTime deletedAt;
+	
+	@OneToOne(cascade = CascadeType.ALL)
+	@JoinColumn(name="category_id")
+	private Category category;
+	
+	@OneToOne(cascade = CascadeType.ALL)
+	@JoinColumn(name="address_id")
+	private PartnerAddress myAddress;
+	
+	
+	
+	
 	
 
 }
