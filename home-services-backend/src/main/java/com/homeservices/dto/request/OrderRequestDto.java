205bfc6383616e699d06568d210f3cd1e61d2e89
Rutvik <rutvikraut567@gmail.com>
2025-08-02 13:03:46 +0530

    Added order service implementation

--- home-services-backend/src/main/java/com/homeservices/dto/request/OrderRequestDto.java
+++ home-services-backend/src/main/java/com/homeservices/dto/request/OrderRequestDto.java
@@ -1,11 +1,14 @@
 package com.homeservices.dto.request;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalTime;
 
-public record OrderRequestDto(Long userId) {
-//	private Long userId;
-//    private Long partnerId;
-//    private Long serviceId;
-//    private LocalDate serviceDate;
-//    private LocalTime serviceTime;
-//    private BigDecimal totalCost;
+public record OrderRequestDto(Long userId,
+		Long partnerId,
+		Long serviceId,
+		LocalDate serviceDate,
+		LocalTime serviceTime,
+		BigDecimal totalCost) {
+
 }
--- home-services-backend/src/main/java/com/homeservices/service/order/OrderService.java
+++ home-services-backend/src/main/java/com/homeservices/service/order/OrderService.java
@@ -1,8 +1,22 @@
 package com.homeservices.service.order;
 
+import java.util.List;
+import java.util.Optional;
+
 import com.homeservices.dto.request.OrderRequestDto;
+import com.homeservices.dto.response.ApiResponse;
 import com.homeservices.entities.Order;
 
 public interface OrderService {
-	Order createOrder(OrderRequestDto dto);
+	ApiResponse createOrder(OrderRequestDto dto);
+
+	Order getOrderById(Long id);
+
+	List<Order> getOrdersByUserId(Long id);
+
+	List<Order> getOrdersByPartnerId(Long id);
+
+	ApiResponse updateOrderStatus(Long id);
+
+	ApiResponse cancelOrderById(Long id);
 }
