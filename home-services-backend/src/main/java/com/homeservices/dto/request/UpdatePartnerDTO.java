c1d9fed0a1b54699f1ec13612a946976edb559e3
Suyog Avhad <ssavhad021@gmail.com>
2025-08-03 16:56:30 +0530

    completed partner Apis

--- home-services-backend/src/main/java/com/homeservices/dto/request/UpdatePartnerDTO.java
+++ home-services-backend/src/main/java/com/homeservices/dto/request/UpdatePartnerDTO.java
@@ -1,24 +1,23 @@
-package com.homeservices.dto.response;
+package com.homeservices.dto.request;
 
 import java.time.LocalDate;
-import java.util.List;
+
+import com.homeservices.dto.response.PartnerAddressDTO;
 
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
-public class PartnerResponseDTO {
+public class UpdatePartnerDTO {
 	private String firstName;
 	private String lastName;
 	private String email;
 	private String phoneNumber;
+	private String password;
 	private LocalDate birthDate;
 	private int experience;
-	private float rating;
-	private int noOfOrders;
-	private double totalEarning;
-	private PartnerCategoryDTO category;
+	private Long categoryId;
 	private PartnerAddressDTO myAddress;
 
 }
--- home-services-backend/src/main/java/com/homeservices/service/partner/PartnerServiceImpl.java
+++ home-services-backend/src/main/java/com/homeservices/service/partner/PartnerServiceImpl.java
@@ -6,14 +6,18 @@ import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.homeservices.custom_exceptions.ResourceNotFoundException;
+import com.homeservices.dao.CategoryRepository;
 import com.homeservices.dao.PartnerRepository;
 import com.homeservices.dto.request.PartnerRequestDTO;
-import com.homeservices.dto.request.VerifyPartnerDTO;
+import com.homeservices.dto.request.UpdatePartnerDTO;
 import com.homeservices.dto.response.ApiResponse;
 import com.homeservices.dto.response.PartnerOrderDTO;
 import com.homeservices.dto.response.PartnerResponseDTO;
 import com.homeservices.dto.response.PartnerServiceDTO;
+import com.homeservices.entities.Category;
 import com.homeservices.entities.Partner;
+import com.homeservices.entities.PartnerAddress;
 
 import lombok.AllArgsConstructor;
 
@@ -23,6 +27,7 @@ import lombok.AllArgsConstructor;
 public class PartnerServiceImpl implements PartnerService {
 
 	private final PartnerRepository partnerRepository;
+	private final CategoryRepository categoryRepository;
 	private ModelMapper mapper;
 
 	@Override
@@ -43,44 +48,83 @@ public class PartnerServiceImpl implements PartnerService {
 
 	@Override
 	public PartnerResponseDTO getPartner(Long id) {
-		// TODO Auto-generated method stub
-		return null;
+		Partner partner = partnerRepository.findById(id)
+				.orElseThrow(() -> new ResourceNotFoundException("Invalid Partner Id.. "));
+		return mapper.map(partner, PartnerResponseDTO.class);
 	}
 
 	@Override
-	public ApiResponse updatePartner(Long id, PartnerRequestDTO partner) {
-		// TODO Auto-generated method stub
-		return null;
+	public ApiResponse updatePartner(Long id, UpdatePartnerDTO partnerupdateDTO) {
+		Partner partner = partnerRepository.findById(id)
+				.orElseThrow(() -> new ResourceNotFoundException("Invalid Partner ID"));
+
+		Category category = categoryRepository.findById(partnerupdateDTO.getCategoryId())
+				.orElseThrow(() -> new ResourceNotFoundException("Invalid Category ID"));
+
+		PartnerAddress myAddress = mapper.map(partnerupdateDTO.getMyAddress(), PartnerAddress.class);
+
+		partner.setFirstName(partnerupdateDTO.getFirstName());
+		partner.setLastName(partnerupdateDTO.getLastName());
+		partner.setEmail(partnerupdateDTO.getEmail());
+		partner.setPhoneNumber(partnerupdateDTO.getPhoneNumber());
+		partner.setExperience(partnerupdateDTO.getExperience());
+		partner.setBirthDate(partnerupdateDTO.getBirthDate());
+		partner.setCategory(category);
+		partner.setMyAddress(myAddress);
+
+		partnerRepository.save(partner);
+
+		return new ApiResponse("Partner Updated with id " + partner.getId());
 	}
 
 	@Override
 	public ApiResponse deletePartner(Long id) {
-		// TODO Auto-generated method stub
-		return null;
+		Partner partner = partnerRepository.findById(id)
+				.orElseThrow(() -> new ResourceNotFoundException("Invalid Partner ID"));
+
+		partner.setStatus(false);
+		partnerRepository.save(partner);
+		return new ApiResponse("Partner Deleted with id" + partner.getId());
 	}
 
 	@Override
 	public List<PartnerOrderDTO> getPartnerOrders(Long id) {
-		// TODO Auto-generated method stub
-		return null;
+		Partner partner = partnerRepository.findById(id)
+				.orElseThrow(() -> new ResourceNotFoundException("Invalid Partner ID"));
+
+		return partner.getMyOrders().stream().map(order -> mapper.map(order, PartnerOrderDTO.class)).toList();
 	}
 
 	@Override
 	public Double getTotalEarning(Long id) {
-		// TODO Auto-generated method stub
-		return null;
+		Partner partner = partnerRepository.findById(id)
+				.orElseThrow(() -> new ResourceNotFoundException("Invalid Partner ID"));
+
+		return partner.getTotalEarning();
 	}
 
 	@Override
 	public List<PartnerServiceDTO> getPartnerServices(Long id) {
-		// TODO Auto-generated method stub
-		return null;
+		Partner partner = partnerRepository.findById(id)
+				.orElseThrow(() -> new ResourceNotFoundException("Invalid Partner ID"));
+		Category c = categoryRepository.findById(partner.getCategory().getId())
+				.orElseThrow(() -> new ResourceNotFoundException("Invalid Category ID"));
+
+		List<PartnerServiceDTO> partnerServices = c.getServices().stream()
+				.map(service -> mapper.map(service, PartnerServiceDTO.class)).toList();
+		if (partnerServices.isEmpty()) {
+			throw new ResourceNotFoundException("No services to show");
+		}
+
+		return partnerServices;
 	}
 
 	@Override
-	public ApiResponse verifyPartner(Long id, VerifyPartnerDTO verifypartnerDTO) {
-		// TODO Auto-generated method stub
-		return null;
+	public ApiResponse verifyPartner(Long id) {
+		Partner partner = partnerRepository.findById(id)
+				.orElseThrow(() -> new ResourceNotFoundException("Invalid Partner ID"));
+		partner.setVerified(true);
+		return new ApiResponse("");
 	}
 
 }
