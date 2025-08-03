f246d994a7f13f5b3ac9206635c56c4331d2d61f
Viranj Masdeakr <viranjrmasdekar@gmail.com>
2025-08-03 14:40:18 +0530

    Modified

---
+++ frontend/src/Components/ServicesPage/BookSlot.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import DatePicker from "../../utils/DatePicker";
+import { Button } from "flowbite-react";
+import { Link } from "react-router-dom";
+
+const BookSlot = () => {
+  return (
+    <div className="flex flex-col items-center mb-5 sticky top-20 self-start">
+      {/* <h1 className="text-2xl font-medium">Book Slot</h1> */}
+      <div className="w-full border border-gray-200 rounded-md flex flex-col p-4 gap-4">
+        <div className="w-full flex justify-center">
+          <DatePicker />
+        </div>
+        <div className="text-center flex-1 flex flex-col justify-between ">
+          <div className="flex flex-col gap-4">
+            <p className="text-xl font-medium">Available Time Slots</p>
+            <div className="grid grid-cols-2 md:grid-cols-3 gap-3">
+              {Array.from({ length: 5 }, (_, i) => {
+                return (
+                  <Button
+                    key={i}
+                    className="px-4 py-2 text-nowrap border rounded bg-white text-black cursor-pointer hover:bg-secondary hover:text-white transition"
+                  >
+                    {`${10 + i}:00 AM`}
+                  </Button>
+                );
+              })}
+            </div>
+          </div>
+
+          <div className="w-full flex justify-end">
+            <Link to={"add-address"}>
+            <Button className="bg-secondary hover:bg-secondary-hover cursor-pointer">
+              Continue
+            </Button>
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default BookSlot;
